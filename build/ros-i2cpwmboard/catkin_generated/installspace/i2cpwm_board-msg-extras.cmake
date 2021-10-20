e066add2a35a077190ef0a41f88c81b1d004c5de
kazuyahirotsu <hrtkzy4444@gmail.com>
2021-10-20 16:18:22 +0900

    Cleaned up

--- build/ros-i2cpwmboard/catkin_generated/installspace/i2cpwm_board-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(i2cpwm_board_MESSAGE_FILES "msg/Servo.msg;msg/ServoArray.msg;msg/ServoConfig.msg;msg/ServoConfigArray.msg;msg/Position.msg;msg/PositionArray.msg")
-set(i2cpwm_board_SERVICE_FILES "srv/IntValue.srv;srv/ServosConfig.srv;srv/DriveMode.srv;srv/StopServos.srv")
