ad36ccf2603f2d1db83546f46624cea31ef0c433
kazuyahirotsu <hrtkzy4444@gmail.com>
2021-10-20 22:59:14 +0900

    Added cv2 with resize function, Cannot see image published by color_detector in web_video_server

--- README.md
+++ README.md
@@ -7,6 +7,9 @@ run this to launch
 ```
 sudo chmod 666 /dev/i2c-1 #raspberry pi
 roslaunch rccar_llc.launch #raspberry pi
+
+rosparam set cv_camera/devide_id 0 #front cam on laptop
+rosparam set cv_camera/device_id 2 #usb cam with laptop
 roslaunch rccar_llc_laptop.launch #laptop
 ```
 catkin_make on raspberry pi
--- color_detector/src/color_detector.py
+++ color_detector/src/color_detector.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 
+import numpy as np
 import rospy
-
-from std_msgs.msg import Int32
+import cv2
+from cv_bridge import CvBridge 
+from sensor_msgs.msg import Image
 
 rospy.init_node('color_detector')
 
-pub = rospy.Publisher('counter', Int32)
+def callback(msg):
+    bridge = CvBridge()
+    cv_array = bridge.imgmsg_to_cv2(msg)
+    #rospy.loginfo(cv_array)
+    resize = cv2.resize(cv_array,dsize=(495,270))
+    cv2.imshow("image",resize)
+    cv2.waitKey(1)
+    msg2 = bridge.cv2_to_imgmsg(resize)
+    #rospy.loginfo(msg2)
 
-rate = rospy.Rate(2)
+sub = rospy.Subscriber('/cv_camera/image_raw', Image, callback)
+pub = rospy.Publisher('msg2', Image)
 
-count = 0
-while not rospy.is_shutdown():
-    pub.publish(count)
-    count += 1
-    rate.sleep()
+rospy.spin()
